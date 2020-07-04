7b2cf0c8369b8188db931cefe71eaa220da45ae2
Aleksei-Zaichenko <aleksei.zaichenko@cix.csi.cuny.edu>
2020-07-04 19:03:58 -0400

    Updates

---
+++ precache-manifest.b77f86cebf41556b489dcad8d874e85d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "effa9b7c670cf03585fe9587a3851737",
+    "url": "/Clocks/index.html"
+  },
+  {
+    "revision": "d37de933b6df731823a3",
+    "url": "/Clocks/static/css/main.854e13d2.chunk.css"
+  },
+  {
+    "revision": "5974cbd6da8a5f2b51c3",
+    "url": "/Clocks/static/js/2.39867c82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clocks/static/js/2.39867c82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d37de933b6df731823a3",
+    "url": "/Clocks/static/js/main.6c3e08b3.chunk.js"
+  },
+  {
+    "revision": "f45aee73ce327e62016c",
+    "url": "/Clocks/static/js/runtime-main.875446ba.js"
+  },
+  {
+    "revision": "1abd8c1a58598974d8415573764ba76a",
+    "url": "/Clocks/static/media/Ozone.1abd8c1a.ttf"
+  }
+]);
